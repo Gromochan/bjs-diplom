2a740181357a33282b1865e2a7d97a2c43c5cb87
Ivan Petrov <kepocka@gmail.com>
2019-04-07 20:32:12 +0300

    Update main.js

--- static/main.js
+++ static/main.js
@@ -2,14 +2,16 @@ class Profile {
     constructor(user, password) {
         this.username = user,
             this.password = password
-        this.authorization
     }
     authorization(username, password) {
+        console.log(`Hello, ${username}. You are ${username}, right?`)
         if (this.username === username && this.password === password) {
+            console.log(`Yes, you are ${username}`)
             return true
         }
         return false
     }
 }
 let user = new Profile("Vasya", "1234")
+user.authorization("Vasya", "1234")
 console.log(user);
