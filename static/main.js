d0714b975b7558fd8bbabeadf4d28dc8ff5695c9
Ivan Petrov <kepocka@gmail.com>
2019-04-08 18:23:34 +0300

    Update main.js

--- static/main.js
+++ static/main.js
@@ -2,6 +2,7 @@ class Profile {
     constructor(user, password) {
         this.username = user,
             this.password = password
+        this.wallet = {}
     }
     authorization(username, password) {
         console.log(`Hello, ${username}. You are ${username}, right?`)
@@ -11,7 +12,19 @@ class Profile {
         }
         return false
     }
+    addCurrency(amount, currency) {
+        this.wallet[currency] = (this.wallet[currency] || 0) + amount
+        return console.log(this.wallet)
+    }
+    convert(amount, currency, coin) {
+        if (this.wallet[currency] > amount) {
+
+        }
+    }
 }
 let user = new Profile("Vasya", "1234")
 user.authorization("Vasya", "1234")
-console.log(user);
+user.addCurrency(1000, "Euro");
+user.addCurrency(1000, "Euro");
+user.addCurrency(10000, "Dollar");
+user.addCurrency(5000, "Dollar")
